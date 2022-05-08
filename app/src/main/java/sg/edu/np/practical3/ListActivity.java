1aeb9f0b60f4f8d377db7142545de5d54706c46c
Lim Hong Ying <s10223298@connect.np.edu.sg>
2022-05-08 22:08:20 +0800

    Added files

---
+++ app/src/main/java/sg/edu/np/practical3/ListActivity.java
@@ -0,0 +1,58 @@
+package sg.edu.np.practical3;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class ListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+
+        ImageView imagebutton = (ImageView) findViewById(R.id.listactivityimage);
+        imagebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(ListActivity.this);
+
+                builder.setTitle("Profile");
+                builder.setMessage("MADness");
+                builder.setCancelable(false);
+                builder.setPositiveButton("View", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Random random = new Random();
+                        int randomint = random.nextInt((99999-0) + 1) + 0;
+
+                        Bundle number = new Bundle();
+                        number.putInt("random", randomint);
+
+                        Intent activityName = new Intent(ListActivity.this, MainActivity.class);
+                        activityName.putExtras(number);
+                        startActivity(activityName);
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+
+                AlertDialog alert = builder.create();
+                alert.show();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/sg/edu/np/practical3/MainActivity.java
@@ -0,0 +1,63 @@
+package sg.edu.np.practical3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Bundle receiving = getIntent().getExtras();
+        int randomnumber = 0;
+        if (receiving != null) {
+            randomnumber = receiving.getInt("random");
+        }
+
+        User example = new User("John", "Test", 1, false);
+
+        TextView username = (TextView) findViewById(R.id.textView3);
+        username.setText(example.getName() + " " + randomnumber);
+
+        Button followbutton = (Button) findViewById(R.id.button3);
+        boolean followed = example.getFollowed();
+        if (followed == false) {
+            followbutton.setText("Follow");
+        }
+
+        else if (followed == true) {
+            followbutton.setText("Unfollow");
+        }
+
+        String name = example.getName();
+        String description = example.getDescription();
+        int id = example.getId();
+        followbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean followed = example.getFollowed();
+                if (followed == false) {
+                    example.setFollowed(true);
+                    followbutton.setText("Unfollow");
+                    Toast.makeText(getApplicationContext(), "Followed", Toast.LENGTH_SHORT).show();
+                }
+
+                else if (followed == true) {
+                    example.setFollowed(false);
+                    followbutton.setText("Follow");
+                    Toast.makeText(getApplicationContext(), "Unfollowed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
